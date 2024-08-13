3600134f9809883aa6101e7ad900af260aaf7565
Alex Hoy <aghoy03@gmail.com>
2024-08-12 21:15:34 -0400

    first attempt at github deployment

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Alex Hoy's portfolio webpage
         </p>
         <a
           className="App-link"
