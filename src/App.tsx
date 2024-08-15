ef8b22a2946cfbb9105de5e695f999545328e4e9
Alex Hoy <aghoy03@gmail.com>
2024-08-15 12:59:26 -0400

    removed some website elements to prepare for adding my own stuff

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,7 @@ function App() {
         <p>
           Alex Hoy's portfolio webpage
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
